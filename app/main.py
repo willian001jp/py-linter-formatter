6a9532f8d2c83dfc7757f804ffdbd417c61cff36
Willian Nomiyama <willian001@gmail.com>
2025-03-18 17:03:43 -0300

    Correction flake8 @mateacademy-ai-mentor

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
-        "path": file_path, "status": "failed"
-        if errors else "passed"}
+        "path": file_path,
+        "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) 
+    return [format_single_linter_file(file_path, errors)
             for file_path, errors in linter_report.items()]
