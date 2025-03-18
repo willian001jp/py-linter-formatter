85e4671b743913f26a42b9e585f5104ca02f49d7
Willian Nomiyama <willian001@gmail.com>
2025-03-18 15:16:42 -0300

    Correction flake8 Return One-line

--- app/main.py
+++ app/main.py
@@ -4,20 +4,16 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8",
-    }
+        "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if errors else "passed",
-    }
+        "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        format_single_linter_file(file_path, errors)
-        for file_path, errors in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
