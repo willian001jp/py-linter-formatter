8344d1c752de426676bcc87a0fa6761acd2029ea
Willian Nomiyama <willian001@gmail.com>
2025-03-18 16:19:20 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,5 +14,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors)
-            for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
